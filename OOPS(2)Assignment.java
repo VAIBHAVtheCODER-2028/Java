d21d294c8ff27935292c6999f9601262a579b8be
VAIBHAVtheCODER-2028 <vs2005005@gmail.com>
2025-11-25 17:12:36 +0530

    Implement Fraction operations and simplify results
    Refactor the Fraction class to include methods for addition, subtraction, and multiplication of fractions, ensuring results are simplified.

--- OOPS(2)Assignment.java
+++ OOPS(2)Assignment.java
@@ -1,3 +1,22 @@
+/*Construct a class Fraction which stores a fraction. It should contain the
+
+-Numerator
+-Denominator
+
+Assume denominator will never be 0.
+
+The class should support the following functionalities
+
+add(Fraction) -> Returns the sum of two fractions
+
+subtract(Fraction) -> Returns the difference of two fractions
+
+multiply(Fraction) -> Returns the product of two fractions
+
+The fraction returned needs to be in the simplest form. If the fraction is p/q then p and q must be co-prime.
+
+You may define any properties in the class as you see appropriate. */
+
 class OOPS_Assignment {
     public static void main(String[] args) {
         Fraction f1=new Fraction(2,3);
@@ -68,4 +87,5 @@ class Fraction {
     void print() {
         System.out.println(numerator + "/" + denominator);
     }
+
 }
